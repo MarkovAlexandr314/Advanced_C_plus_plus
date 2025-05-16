8ccde2975b734380a34ec701eba19978689b5e47
MarkovAlexandr314 <markov314@bk.ru>
2025-05-16 13:56:11 +0300

    hot change

--- task3/TypeMap.h
+++ task3/TypeMap.h
@@ -1,12 +1,13 @@
 #include "../task2/type_list.h"
 #include <tuple>
-#include <stdexcept> // Для std::runtime_error
+#include <stdexcept>
 
 template <typename... Types>
 class TypeMap {
 private:
     template <typename T>
-    struct Element {
+    struct Element
+    {
         T value{};
         bool is_present = false;
     };
@@ -17,13 +18,15 @@ private:
     using List = detail::TypeList<Types...>;
 
     template <typename T>
-    static constexpr size_t GetIndex() {
+    static constexpr size_t GetIndex()
+    {
         return detail::IndexOf<T, List>::value;
     }
 
 public:
     template <typename T>
-    void AddValue(T value) {
+    void AddValue(T value)
+    {
         static_assert(GetIndex<T>() != static_cast<size_t>(-1), "Type not in TypeMap");
         auto& elem = std::get<GetIndex<T>()>(storage_);
         elem.value = value;
@@ -31,33 +34,39 @@ public:
     }
 
     template <typename T>
-    T& GetValue() {
+    T& GetValue()
+    {
         static_assert(GetIndex<T>() != static_cast<size_t>(-1), "Type not in TypeMap");
         auto& elem = std::get<GetIndex<T>()>(storage_);
-        if (!elem.is_present) {
+        if (!elem.is_present)
+        {
             throw std::runtime_error("Element not present");
         }
         return elem.value;
     }
 
     template <typename T>
-    const T& GetValue() const {
+    const T& GetValue() const
+    {
         static_assert(GetIndex<T>() != static_cast<size_t>(-1), "Type not in TypeMap");
         const auto& elem = std::get<GetIndex<T>()>(storage_);
-        if (!elem.is_present) {
+        if (!elem.is_present)
+        {
             throw std::runtime_error("Element not present");
         }
         return elem.value;
     }
 
     template <typename T>
-    bool Contains() const {
+    bool Contains() const
+    {
         static_assert(GetIndex<T>() != static_cast<size_t>(-1), "Type not in TypeMap");
         return std::get<GetIndex<T>()>(storage_).is_present;
     }
 
     template <typename T>
-    void RemoveValue() {
+    void RemoveValue()
+    {
         static_assert(GetIndex<T>() != static_cast<size_t>(-1), "Type not in TypeMap");
         auto& elem = std::get<GetIndex<T>()>(storage_);
         elem.is_present = false;
